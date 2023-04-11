021a38c2122756c6ac494aa73bb5ae32811ae37f
jose-omar-landa <jlanda4@wgu.edu>
2023-04-11 18:21:07 -0500

    created "Add Health History" button on HealthHistoryPage.java that starts the AddHealthHistory activity

--- app/src/main/java/com/example/healthmanagerapp/HealthHistoryPage.java
+++ app/src/main/java/com/example/healthmanagerapp/HealthHistoryPage.java
@@ -2,13 +2,29 @@ package com.example.healthmanagerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HealthHistoryPage extends AppCompatActivity {
+
+    ImageButton addHealthProblem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_history_page);
+
+        addHealthProblem = findViewById(R.id.addHealthHistButton);
+
+        addHealthProblem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HealthHistoryPage.this, AddHealthHistoryPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
